20e2d167cb3844ee70ae9c7b47c9c11e9b13de07
Michelle Scott <scottmm374@gmail.com>
2019-09-12 17:52:38 -0400

    Everything rendering on page, functionality done

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { withFormik, Form, Field} from 'formik';
+import React, { useState, useEffect } from 'react';
+import { withFormik, Form, Field } from 'formik';
 import axios from 'axios';
 import * as Yup from 'yup';
 
 
-function LoginForm ({values, errors, touched, isSubmitting}) {
+function LoginForm({ values, errors, touched, isSubmitting, status }) {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        if (status) {
+            setUsers([...users, status])
+        }
+    }, [status])
+
     return (
         <Form>
             <div>
@@ -21,15 +29,24 @@ function LoginForm ({values, errors, touched, isSubmitting}) {
             </div>
             <label>
                 <Field type="checkbox" name="tos" checked={values.tos} />
-                Agree to terms of service 
+                Agree to terms of service
             </label>
             <button disabled={isSubmitting}>Submit</button>
+
+            {users.map((user) => (
+                <div>
+                    {/* key={user.id} */}
+                    <div>Name: {user.name}</div>
+                    <div>Email: {user.email}</div>
+                </div>
+            ))}
+
         </Form>
     );
 }
 
-const FormikForm = withFormik ({
-    mapPropsToValues({ name, email, password, tos}) {
+const FormikForm = withFormik({
+    mapPropsToValues({ name, email, password, tos }) {
         return {
             name: name || "",
             email: email || "",
@@ -37,28 +54,29 @@ const FormikForm = withFormik ({
             tos: tos || false
         };
     },
-    
+
     // Validation user input 
 
     validationSchema: Yup.object().shape({
         name: Yup.string().required("Required"),
         email: Yup.string().email("Email not valid").required("Required"),
         password: Yup.string().min(6, "Password must be at least 6 characters long").required("Required"),
-            // want to add required for tos
+        // want to add required for tos
     }),
 
-   
+
 
     // handeling user input    
 
-    handleSubmit(values, { resetForm, setSubmitting }) {
+    handleSubmit(values, { resetForm, setSubmitting, setStatus }) {
         axios
             .post(' https://reqres.in/api/users', values)
             .then(res => {
                 console.log("res", res);
+                setStatus(res.data);
                 resetForm();
                 setSubmitting(false);
-           })
+            })
             .catch(err => {
                 console.log("err", err);
                 setSubmitting(false);
@@ -66,6 +84,6 @@ const FormikForm = withFormik ({
     }
 
 })(LoginForm)
-    
+
 
 export default FormikForm;
